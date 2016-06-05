05621e0f82e493b5ac81ed90e196af334841f8b8
RenePeuser - Rene Peuser <rene.peuser@hotmail.de>
2016-06-05 18:06:11 +0200

    Initlize implementation

---
+++ TypeToDataTemplateSelector/TypeToDataTemplateSelector/TypeToDataTemplate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Windows;
+
+namespace TypeToDataTemplateSelector
+{
+    public class TypeToDataTemplate
+    {
+        public Type Type { get; set; }
+
+        public DataTemplate DataTemplate { get; set; }
+    }
+}
---
+++ TypeToDataTemplateSelector/TypeToDataTemplateSelector/TypeToDataTemplateSelector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TypeToDataTemplateSelector
+{
+    public class TypeDataTemplateSelector : DataTemplateSelector
+    {
+        public List<TypeToDataTemplate> TypeToDataTemplates { get; set; }
+
+        public TypeDataTemplateSelector()
+        {
+            TypeToDataTemplates = new List<TypeToDataTemplate>();
+        }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            var type = item.GetType();
+            var typeToDataTemplate = TypeToDataTemplates.FirstOrDefault(t => t.Type == type);
+            return typeToDataTemplate?.DataTemplate;
+        }
+    }
+}
